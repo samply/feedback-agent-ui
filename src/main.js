150faecd935127ae9cec7ee6c4754bbfcd3c1af9
Gerhard Salvini <gerhard@ubuntu.com>
2025-01-24 10:42:35 +0100

    Removed Single Spa code
    Make this into a simple standalone app for the piloting phase.
    
    Single Spa can easily be added back once development is complete.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,8 @@
-import { h, createApp } from 'vue';
-import singleSpaVue from 'single-spa-vue';
+import { createApp } from 'vue';
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
 import App from './App.vue';
 
-const vueLifecycles = singleSpaVue({
-  createApp,
-  appOptions: {
-    render() {
-      return h(App, {
-        // single-spa props are available on the "this" object. Forward them to your component as needed.
-        // https://single-spa.js.org/docs/building-applications#lifecycle-props
-        // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
-        /*
-        name: this.name,
-        mountParcel: this.mountParcel,
-        singleSpa: this.singleSpa,
-        */
-      });
-    },
-  },
-});
+const app = createApp(App);
+app.mount('#app');
 
-export const bootstrap = vueLifecycles.bootstrap;
-export const mount = vueLifecycles.mount;
-export const unmount = vueLifecycles.unmount;
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,3 @@
-/*
-const { defineConfig } = require('@vue/cli-service')
-
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-*/
 module.exports = {
-  configureWebpack: {
-    devtool: 'source-map',
-    output: {
-      libraryTarget: "system",
-    },
-  },
-  chainWebpack: (config) => {
-    if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) {
-      config.plugins.delete("SystemJSPublicPathWebpackPlugin");
-    }
-  },
   filenameHashing: false,
 };
